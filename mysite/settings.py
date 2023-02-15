cd2aaf5dca6262c3cf10bd551e9ab387a1c71d1a
YuliaSukach <yuliyasukach123@gmail.com>
2023-02-15 10:57:59 +0100

    debug is false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kfu+q7r*8a7d-ym_1lw!4rimz5*1z#qm&$pv6u3bj5pobdf^!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
